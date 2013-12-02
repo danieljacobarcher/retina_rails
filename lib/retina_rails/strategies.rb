c5af134ceba35dc5372175144a55e83eb257489c
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-12-01 23:13:12 -0800

    Update strategies.rb
    Modified for Mongoid

--- lib/retina_rails/strategies.rb
+++ lib/retina_rails/strategies.rb
@@ -9,7 +9,7 @@ module RetinaRails
         ::CarrierWave::Uploader::Base.send(:include, RetinaRails::Strategies::CarrierWave::Base)
       end
       if defined?(::Paperclip)
-        ::ActiveRecord::Base.send(:include, RetinaRails::Strategies::Paperclip::Base)
+        ::Mongoid::Document.send(:include, RetinaRails::Strategies::Paperclip::Base)
       end
     end
 
